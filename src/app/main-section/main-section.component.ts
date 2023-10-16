2ad536791fcfe555e03650554f3d82f85b25bbd8
Lucile Jones <jones.lucile@gmail.com>
2023-10-16 15:20:53 -0600

    changed css for displaying the add task form

--- src/app/main-section/main-section.component.ts
+++ src/app/main-section/main-section.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-section.component.css']
 })
 export class MainSectionComponent {
-  onAddFormClicked = true;
+  onAddFormClicked = false;
 }
